7e0ed3035db54d00c56322debc587d6c2c4e68f2
horiondreher <horiondreher@gmail.com>
2023-10-10 00:21:10 -0300

    Creating a http server with gin and adding env variables

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
-package simple_bank
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+
+	"github.com/horiondreher/simple_bank/api"
+	db "github.com/horiondreher/simple_bank/db/sqlc"
+	"github.com/horiondreher/simple_bank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
