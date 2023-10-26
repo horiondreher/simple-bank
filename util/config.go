f41d54a424a8098eefea8c5d3068fb9e5576cf17
horiondreher <horiondreher@gmail.com>
2023-10-25 23:44:53 -0300

    Adding login endpoint

--- util/config.go
+++ util/config.go
@@ -1,11 +1,17 @@
 package util
 
-import "github.com/spf13/viper"
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER" default:"postgres"`
-	DBSource      string `mapstructure:"DB_SOURCE" default:"postgresql://pguser:pgpassword@localhost:5432/simple-bank?sslmode=disable"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS" default:"localhost:8080"`
+	DBDriver            string        `mapstructure:"DB_DRIVER" default:"postgres"`
+	DBSource            string        `mapstructure:"DB_SOURCE" default:"postgresql://pguser:pgpassword@localhost:5432/simple-bank?sslmode=disable"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS" default:"localhost:8080"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
